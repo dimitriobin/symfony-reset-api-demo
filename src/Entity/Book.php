4753d4a105c08b73831244c99f1857ecb5351201
Dimitri Obin <dimitriobin@gmail.com>
2023-05-01 23:17:49 +0200

    update book entity to work with authors

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 class Book
@@ -11,10 +12,16 @@ class Book
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['getBooks', 'getAuthors'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['getBooks', 'getAuthors'])]
     private ?string $title = null;
+
+    #[ORM\ManyToOne(inversedBy: 'books')]
+    #[Groups(['getBooks'])]
+    private ?Author $author = null;
 
     public function getId(): ?int
     {
@@ -29,6 +36,18 @@ class Book
     public function setTitle(string $title): self
     {
         $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Author $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
